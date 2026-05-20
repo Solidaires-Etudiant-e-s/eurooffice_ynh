4efddab6692d923786ee349c176ad06ab867be98
otm33 <b.arrierg@orange.fr>
2026-05-20 22:42:57 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -77,11 +77,11 @@ setup_sources() {
     # We use sources in order to recompile binary
     if [[ "$rebuild_without_limitations" == "1" ]] ; then
         ynh_setup_source --source_id="src" --dest_dir="$install_dir/src"
-     #   ynh_replace --match="const buildVersion = " --replace="const buildVersion = '${YNH_APP_MANIFEST_VERSION%%~*}';" --file="$install_dir/src/Common/sources/commondefines.js"
+        ynh_replace --match="const buildVersion = " --replace="const buildVersion = '${YNH_APP_MANIFEST_VERSION%%~*}';" --file="$install_dir/src/Common/sources/commondefines.js"
     # no longer any tags or releases for "sources.src", only a commit number therefore align its version with main. Is it the more convenient ?
     #buildNumber=$(ynh_read_manifest "resources.sources.src.url"| sed "s/\.tar\.gz//" | grep -Eo "[0-9]+$")
-    #buildNumber='${YNH_APP_MANIFEST_VERSION%%~*}'
-      #  ynh_replace --match="const buildNumber = " --replace="const buildNumber = $buildNumber;" --file="$install_dir/src/Common/sources/commondefines.js"
+    buildNumber='9.4.0.129'
+        ynh_replace --match="const buildNumber = " --replace="const buildNumber = $buildNumber;" --file="$install_dir/src/Common/sources/commondefines.js"
         ynh_replace --match="const buildDate = " --replace="const buildDate = '$( date +%F )';" --file="$install_dir/src/Common/sources/license.js"
     fi
     set_permissions
