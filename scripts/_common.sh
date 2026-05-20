c4f93b07f54347edd6c5f0f587074ac7733504f6
otm33 <b.arrierg@orange.fr>
2026-05-20 19:11:07 +0200

    Update buildNumber assignment in _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -78,7 +78,9 @@ setup_sources() {
     if [[ "$rebuild_without_limitations" == "1" ]] ; then
         ynh_setup_source --source_id="src" --dest_dir="$install_dir/src"
         ynh_replace --match="const buildVersion = " --replace="const buildVersion = '${YNH_APP_MANIFEST_VERSION%%~*}';" --file="$install_dir/src/Common/sources/commondefines.js"
-    buildNumber=$(ynh_read_manifest "resources.sources.src.url"| sed "s/\.tar\.gz//" | grep -Eo "[0-9]+$")
+    # no longer any tags or releases for "sources.src", only a commit number therefore align its version with main
+    #buildNumber=$(ynh_read_manifest "resources.sources.src.url"| sed "s/\.tar\.gz//" | grep -Eo "[0-9]+$")
+    buildNumber='${YNH_APP_MANIFEST_VERSION%%~*}'
         ynh_replace --match="const buildNumber = " --replace="const buildNumber = $buildNumber;" --file="$install_dir/src/Common/sources/commondefines.js"
         ynh_replace --match="const buildDate = " --replace="const buildDate = '$( date +%F )';" --file="$install_dir/src/Common/sources/license.js"
     fi
